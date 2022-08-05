c183af9588845eb81aeda633a6f7c6c0da3c3208
Ali Saadat <ali.saadat.4950@gmail.com>
2022-08-05 13:58:35 +0430

    Add >> 451A

---
+++ Accepted/ProblemSet/451A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+#define def 10000 
+#define forn(i,n) for(int i=0;i<n;i++) 
+#define pb push_back 
+#define F first 
+#define S second 
+#define mp make_pair 
+#define elif else if 
+typedef long long int intl; 
+typedef vector<intl> vci;
+typedef pair<int,int> pii; 
+typedef vector<pii> vpii; 
+typedef vector<string> vs; 
+typedef vector<bool> vb; 
+typedef vector<double> vd; 
+
+int main (void)
+{
+    intl n, m;
+    cin >> n >> m;
+
+    intl a = min(n, m);
+    if(a % 2 != 0) cout << "Akshat";
+    else cout << "Malvika";
+}
